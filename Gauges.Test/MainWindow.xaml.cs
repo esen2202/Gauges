a92317b1e7f8aff721a973265755fd374f2d16d1
Esen Erkan <erkanesen2202@gmail.com>
2019-06-27 21:45:39 +0300

    radar animation

--- Gauges.Test/MainWindow.xaml.cs
+++ Gauges.Test/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,34 @@ namespace Gauges.Test
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Storyboard sb = new Storyboard();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnStartRadar_Click(object sender, RoutedEventArgs e)
+        {   
+            DoubleAnimation da = new DoubleAnimation(0, 360, TimeSpan.FromMilliseconds(1300));
+
+            //SineEase easingFunction = new SineEase();
+            //easingFunction.EasingMode = EasingMode.EaseIn;
+            //da.EasingFunction = easingFunction;
+
+            Storyboard.SetTarget(da, this.GaugePolar1);
+            Storyboard.SetTargetProperty(da, new PropertyPath(Gauges.Polar.GaugePolar.RadarAngleProperty));
+            sb.Children.Add(da);
+   
+            sb.RepeatBehavior = RepeatBehavior.Forever;
+
+            sb.Begin();
+        }
+
+        private void BtnStopRadar_Click(object sender, RoutedEventArgs e)
+        {
+            sb?.Stop();
         }
     }
 }
